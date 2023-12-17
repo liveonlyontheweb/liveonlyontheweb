e46d1214467d937de28ca79b68a2a233e46509e8
Oleg Angulyarov <4nglrv@gmail.com>
2023-12-17 14:42:04 +0300

    Changed accent color

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,11 +1,10 @@
-import { FunctionComponent } from "react";
-import classNames from "classnames";
+import classNames from 'classnames';
+import { FunctionComponent } from 'react';
 
-import classes from "./SocialLink.module.css";
-
-import SoundCloudLogo from "../assets/icomoon-free_soundcloud2.svg?react";
-import InstagramLogo from "../assets/formkit_instagram.svg?react";
-import VKLogo from "../assets/fa6-brands_vk.svg?react";
+import VKLogo from '../assets/fa6-brands_vk.svg?react';
+import InstagramLogo from '../assets/formkit_instagram.svg?react';
+import SoundCloudLogo from '../assets/icomoon-free_soundcloud2.svg?react';
+import classes from './SocialLink.module.css';
 
 // TODO: Add spring animations
 // https://www.react-spring.dev/
@@ -16,18 +15,18 @@ const SocialLinks = () => {
     logo: FunctionComponent;
   }> = [
     {
-      href: "https://soundcloud.com/liveonlyontheweb",
-      text: "SoundCloud",
+      href: 'https://soundcloud.com/liveonlyontheweb',
+      text: 'SoundCloud',
       logo: SoundCloudLogo,
     },
     {
-      href: "https://www.instagram.com/liveonlyontheweb/",
-      text: "Instagram",
+      href: 'https://www.instagram.com/liveonlyontheweb/',
+      text: 'Instagram',
       logo: InstagramLogo,
     },
     {
-      href: "https://vk.com/liveonlyontheweb",
-      text: "VK",
+      href: 'https://vk.com/liveonlyontheweb',
+      text: 'VK',
       logo: VKLogo,
     },
   ];
@@ -36,7 +35,7 @@ const SocialLinks = () => {
     <div className={classes.social_links}>
       {links.map((link, index) => (
         <a
-          className={classNames(classes.link_wrapper, "text-sm text-bold")}
+          className={classNames(classes.link_wrapper, 'text-sm text-bold')}
           href={link.href}
           key={index}
         >
